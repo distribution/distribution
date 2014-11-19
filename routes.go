5789ca7629cd147f04ecbe922b5abc602a121d98
Stephen J Day <stephen.day@docker.com>
2014-11-18 19:38:14 -0800

    Update routes to reflect specification changes
    To bring the implementation inline with the specification, the names and
    structure of the API routes have been updated.
    
    The overloaded term "image" has been replaced with the term "manifest", which
    may also be known as "image manifest".  The desire for the layer storage to be
    more of a general blob storage is reflected in moving from "layer" api prefixes
    to "blob". The "tarsum" path parameter has been replaced by a more general
    "digest" parameter and is no longer required to start uploads. Another set of
    changes will come along to support this change at the storage service layer.

--- routes.go
+++ routes.go
@@ -6,19 +6,19 @@ import (
 )
 
 const (
-	routeNameImageManifest     = "image-manifest"
-	routeNameTags              = "tags"
-	routeNameLayer             = "layer"
-	routeNameLayerUpload       = "layer-upload"
-	routeNameLayerUploadResume = "layer-upload-resume"
+	routeNameImageManifest    = "image-manifest"
+	routeNameTags             = "tags"
+	routeNameBlob             = "blob"
+	routeNameBlobUpload       = "blob-upload"
+	routeNameBlobUploadResume = "blob-upload-resume"
 )
 
 var allEndpoints = []string{
 	routeNameImageManifest,
 	routeNameTags,
-	routeNameLayer,
-	routeNameLayerUpload,
-	routeNameLayerUploadResume,
+	routeNameBlob,
+	routeNameBlobUpload,
+	routeNameBlobUploadResume,
 }
 
 // v2APIRouter builds a gorilla router with named routes for the various API
@@ -27,11 +27,11 @@ func v2APIRouter() *mux.Router {
 	router := mux.NewRouter().
 		StrictSlash(true)
 
-	// GET      /v2/<name>/image/<tag>	Image Manifest	Fetch the image manifest identified by name and tag.
-	// PUT      /v2/<name>/image/<tag>	Image Manifest	Upload the image manifest identified by name and tag.
-	// DELETE   /v2/<name>/image/<tag>	Image Manifest	Delete the image identified by name and tag.
+	// GET      /v2/<name>/manifest/<tag>	Image Manifest	Fetch the image manifest identified by name and tag.
+	// PUT      /v2/<name>/manifest/<tag>	Image Manifest	Upload the image manifest identified by name and tag.
+	// DELETE   /v2/<name>/manifest/<tag>	Image Manifest	Delete the image identified by name and tag.
 	router.
-		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/image/{tag:" + common.TagNameRegexp.String() + "}").
+		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/manifest/{tag:" + common.TagNameRegexp.String() + "}").
 		Name(routeNameImageManifest)
 
 	// GET	/v2/<name>/tags/list	Tags	Fetch the tags under the repository identified by name.
@@ -39,22 +39,22 @@ func v2APIRouter() *mux.Router {
 		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/tags/list").
 		Name(routeNameTags)
 
-	// GET	/v2/<name>/layer/<tarsum>	Layer	Fetch the layer identified by tarsum.
+	// GET	/v2/<name>/blob/<digest>	Layer	Fetch the blob identified by digest.
 	router.
-		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/layer/{tarsum:" + common.TarsumRegexp.String() + "}").
-		Name(routeNameLayer)
+		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/blob/{digest:[a-zA-Z0-9-_+.]+:[a-zA-Z0-9-_+.=]+}").
+		Name(routeNameBlob)
 
-	// POST	/v2/<name>/layer/<tarsum>/upload/	Layer Upload	Initiate an upload of the layer identified by tarsum. Requires length and a checksum parameter.
+	// POST	/v2/<name>/blob/upload/	Layer Upload	Initiate an upload of the layer identified by tarsum.
 	router.
-		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/layer/{tarsum:" + common.TarsumRegexp.String() + "}/upload/").
-		Name(routeNameLayerUpload)
+		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/blob/upload/").
+		Name(routeNameBlobUpload)
 
-	// GET	/v2/<name>/layer/<tarsum>/upload/<uuid>	Layer Upload	Get the status of the upload identified by tarsum and uuid.
-	// PUT	/v2/<name>/layer/<tarsum>/upload/<uuid>	Layer Upload	Upload all or a chunk of the upload identified by tarsum and uuid.
-	// DELETE	/v2/<name>/layer/<tarsum>/upload/<uuid>	Layer Upload	Cancel the upload identified by layer and uuid
+	// GET	/v2/<name>/blob/upload/<uuid>	Layer Upload	Get the status of the upload identified by tarsum and uuid.
+	// PUT	/v2/<name>/blob/upload/<uuid>	Layer Upload	Upload all or a chunk of the upload identified by tarsum and uuid.
+	// DELETE	/v2/<name>/blob/upload/<uuid>	Layer Upload	Cancel the upload identified by layer and uuid
 	router.
-		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/layer/{tarsum:" + common.TarsumRegexp.String() + "}/upload/{uuid}").
-		Name(routeNameLayerUploadResume)
+		Path("/v2/{name:" + common.RepositoryNameRegexp.String() + "}/blob/upload/{uuid}").
+		Name(routeNameBlobUploadResume)
 
 	return router
 }
