0d1305268934cb3ba838356b31b60f11ed6972e0
Stephen J Day <stephen.day@docker.com>
2015-03-24 17:29:09 -0700

    Experiment with calling the registry a namespace
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- registry.go
+++ registry.go
@@ -2,12 +2,35 @@ package distribution
 
 import "golang.org/x/net/context"
 
-// Registry represents a collection of repositories, addressable by name.
-type Registry interface {
+// Scope defines the set of items that match a namespace.
+type Scope interface {
+	// Contains returns true if the name belongs to the namespace.
+	Contains(name string) (bool, error)
+}
+
+// Namespace represents a collection of repositories, addressable by name.
+// Generally, a namespace is backed by a set of one or more services,
+// providing facilities such as registry access, trust, and indexing.
+type Namespace interface {
+	// Scope describes the names that can be used with this Namespace. The
+	// global namespace will have a scope that matches all names. The scope
+	// effectively provides an identity for the namespace.
+	Scope() Scope
+
 	// Repository should return a reference to the named repository. The
 	// registry may or may not have the repository but should always return a
 	// reference.
 	Repository(ctx context.Context, name string) (Repository, error)
+
+	// TODO(stevvooe): Namespaces should provide other services that allow one
+	// to explore the contents of the namespace. One such example is an
+	// "index" or search service. A commented example is provided here to
+	// illustrate the concept.
+	//
+	// Index returns an IndexService that provides search across the
+	// namespace, supporting content discovery and collective information
+	// about available images.
+	// Index(ctx context.Context) (IndexService, error)
 }
 
 // Repository is a named collection of manifests and layers.
