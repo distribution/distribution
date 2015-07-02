645a134a5bda9d496c9c40ed2971744ca35e55d9
Dave Tucker <dave@dtucker.co.uk>
2015-07-02 22:01:15 +0100

    Add PingChecker
    This health checker is configured with two criteria
    - The IPv4 or IPv6 address of an endpoint to ping
    - The Maximum Round-Trip time in milliseconds
    
    A service is disabled if the IP address is unreachable, or the maximum
    RTT is exceeded
    
    Fixes #273
    
    Signed-off-by: Dave Tucker <dt@docker.com>

--- health/checks/checks.go
+++ health/checks/checks.go
@@ -2,10 +2,20 @@ package checks
 
 import (
 	"errors"
-	"github.com/docker/distribution/health"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/docker/distribution/health"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/internal/iana"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
 )
+
+const timeout time.Duration = 3 * time.Second
 
 // FileChecker checks the existence of a file and returns and error
 // if the file exists, taking the application out of rotation
@@ -32,4 +42,117 @@ func HTTPChecker(r string) health.Checker {
 		}
 		return nil
 	})
+}
+
+// PingChecker sends an ICMP Echo request and verifies that a
+// response was received within the maximum round-trip time,
+// taking the application out of rotation otherwise. The maxRTT
+// must be less than 3 seconds
+func PingChecker(a string, maxRTT time.Duration) health.Checker {
+	return health.CheckFunc(func() error {
+		ip := net.ParseIP(a)
+		if ip == nil {
+			return fmt.Errorf("%s is not a valid IP address", a)
+		}
+		b := ip.To4()
+		pingFunc := pingIPv4
+		if b == nil {
+			pingFunc = pingIPv6
+		}
+		rtt, err := pingFunc(ip)
+		if err != nil {
+			return fmt.Errorf("ping request unsuccessful: %v", err)
+		}
+		if rtt > maxRTT {
+			return fmt.Errorf("maximum RTT of %v exceeded. measured RTT: %v", maxRTT, rtt)
+		}
+		return nil
+	})
+}
+
+func pingIPv4(ip net.IP) (time.Duration, error) {
+	c, err := icmp.ListenPacket("udp4", "0.0.0.0")
+	if err != nil {
+		return 0, err
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("I CAN HAZ REPLY?"),
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		return 0, err
+	}
+	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: ip}); err != nil {
+		return 0, err
+	}
+
+	rb := make([]byte, 1500)
+	start := time.Now()
+	deadline := start.Add(timeout)
+	c.SetReadDeadline(deadline)
+	n, _, err := c.ReadFrom(rb)
+	if err != nil {
+		return 0, err
+	}
+	rtt := time.Since(start)
+	rm, err := icmp.ParseMessage(iana.ProtocolICMP, rb[:n])
+	if err != nil {
+		return 0, err
+	}
+	switch rm.Type {
+	case ipv4.ICMPTypeEchoReply:
+		return rtt, nil
+	default:
+		return 0, fmt.Errorf("got %v; wanted an echo reply", rm)
+	}
+}
+
+func pingIPv6(ip net.IP) (time.Duration, error) {
+	c, err := icmp.ListenPacket("udp6", "::")
+	if err != nil {
+		return 0, err
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv6.ICMPTypeEchoRequest, Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff, Seq: 1,
+			Data: []byte("I CAN HAZ REPLY?"),
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		return 0, err
+	}
+	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: ip}); err != nil {
+		return 0, err
+	}
+
+	rb := make([]byte, 1500)
+	start := time.Now()
+	deadline := start.Add(timeout)
+	c.SetReadDeadline(deadline)
+	n, _, err := c.ReadFrom(rb)
+	if err != nil {
+		return 0, err
+	}
+	rtt := time.Since(start)
+	rm, err := icmp.ParseMessage(iana.ProtocolIPv6ICMP, rb[:n])
+	if err != nil {
+		return 0, err
+	}
+	switch rm.Type {
+	case ipv6.ICMPTypeEchoReply:
+		return rtt, nil
+	default:
+		return 0, fmt.Errorf("got %v; wanted an echo reply", rm)
+	}
+
 }
