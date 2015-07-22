9423b38d5f8b43400ebf9787f0b13a2b7a639632
Aaron Lehmann <aaron.lehmann@docker.com>
2015-07-21 17:10:36 -0700

    Add additional test coverage for the regexp contained in RepositoryNameRegexp
    This was inspired by problems found with new regexps proposed in PR #690
    
    Signed-off-by: Aaron Lehmann <aaron.lehmann@docker.com>

--- registry/api/v2/names_test.go
+++ registry/api/v2/names_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestRepositoryNameRegexp(t *testing.T) {
+func TestRepositoryComponentNameRegexp(t *testing.T) {
 	for _, testcase := range []struct {
 		input string
 		err   error
@@ -145,6 +145,146 @@ func TestRepositoryNameRegexp(t *testing.T) {
 				} else {
 					failf("unexpected error validating repository name: %v", err)
 				}
+			}
+		}
+	}
+}
+
+func TestRepositoryNameRegexp(t *testing.T) {
+	for _, testcase := range []struct {
+		input   string
+		invalid bool
+	}{
+		{
+			input: "short",
+		},
+		{
+			input: "simple/name",
+		},
+		{
+			input: "library/ubuntu",
+		},
+		{
+			input: "docker/stevvooe/app",
+		},
+		{
+			input: "aa/aa/aa/aa/aa/aa/aa/aa/aa/bb/bb/bb/bb/bb/bb",
+		},
+		{
+			input: "aa/aa/bb/bb/bb",
+		},
+		{
+			input: "a/a/a/b/b",
+		},
+		{
+			input:   "a/a/a/a/",
+			invalid: true,
+		},
+		{
+			input:   "a//a/a",
+			invalid: true,
+		},
+		{
+			input: "a",
+		},
+		{
+			input: "a/aa",
+		},
+		{
+			input: "aa/a",
+		},
+		{
+			input: "a/aa/a",
+		},
+		{
+			input:   "foo.com/",
+			invalid: true,
+		},
+		{
+			// currently not allowed by the regex
+			input:   "foo.com:8080/bar",
+			invalid: true,
+		},
+		{
+			input: "foo.com/bar",
+		},
+		{
+			input: "foo.com/bar/baz",
+		},
+		{
+			input: "foo.com/bar/baz/quux",
+		},
+		{
+			input: "blog.foo.com/bar/baz",
+		},
+		{
+			input: "asdf",
+		},
+		{
+			input:   "asdf$$^/aa",
+			invalid: true,
+		},
+		{
+			input: "aa-a/aa",
+		},
+		{
+			input: "aa/aa",
+		},
+		{
+			input: "a-a/a-a",
+		},
+		{
+			input:   "a-/a/a/a",
+			invalid: true,
+		},
+		{
+			input:   "-foo/bar",
+			invalid: true,
+		},
+		{
+			input:   "foo/bar-",
+			invalid: true,
+		},
+		{
+			input:   "foo-/bar",
+			invalid: true,
+		},
+		{
+			input:   "foo/-bar",
+			invalid: true,
+		},
+		{
+			input:   "_foo/bar",
+			invalid: true,
+		},
+		{
+			input:   "foo/bar_",
+			invalid: true,
+		},
+		{
+			input:   "____/____",
+			invalid: true,
+		},
+		{
+			input:   "_docker/_docker",
+			invalid: true,
+		},
+		{
+			input:   "docker_/docker_",
+			invalid: true,
+		},
+	} {
+		failf := func(format string, v ...interface{}) {
+			t.Logf(strconv.Quote(testcase.input)+": "+format, v...)
+			t.Fail()
+		}
+
+		matches := RepositoryNameRegexp.FindString(testcase.input) == testcase.input
+		if matches == testcase.invalid {
+			if testcase.invalid {
+				failf("expected invalid repository name %s", testcase.input)
+			} else {
+				failf("expected valid repository name %s", testcase.input)
 			}
 		}
 	}
