9f9a7f230b6a7ee87dfbaaf9fb45ccd175764c03
Stephen J Day <stephen.day@docker.com>
2015-07-23 23:16:27 -0700

    Allow disabling of starage driver redirects
    Storage drivers can implement a method called URLFor which can return a direct
    url for a given path. The functionality allows the registry to direct clients
    to download content directly from the backend storage. This is commonly used
    with s3 and cloudfront. Under certain conditions, such as when the registry is
    not local to the backend, these redirects can hurt performance and waste
    incoming bandwidth on pulls. This feature addition allows one to disable this
    feature, if required.
    
    Signed-off-by: Stephen J Day <stephen.day@docker.com>
    
    Conflicts:
    	configuration/configuration.go
    	registry/handlers/app.go
    	registry/storage/catalog_test.go
    	registry/storage/manifeststore_test.go
    	registry/storage/registry.go

--- registry/storage/catalog_test.go
+++ registry/storage/catalog_test.go
@@ -22,7 +22,7 @@ func setupFS(t *testing.T) *setupEnv {
 	d := inmemory.New()
 	c := []byte("")
 	ctx := context.Background()
-	registry := NewRegistryWithDriver(ctx, d, memory.NewInMemoryBlobDescriptorCacheProvider(), false)
+	registry := NewRegistryWithDriver(ctx, d, memory.NewInMemoryBlobDescriptorCacheProvider(), false, true)
 	rootpath, _ := defaultPathMapper.path(repositoriesRootPathSpec{})
 
 	repos := []string{
