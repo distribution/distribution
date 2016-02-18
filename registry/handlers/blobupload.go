21a08786651b4c0ef73862dadda7729ebef6938f
Michal Minar <miminar@redhat.com>
2016-02-18 19:20:53 +0100

    Commit blob with known size
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- registry/handlers/blobupload.go
+++ registry/handlers/blobupload.go
@@ -239,12 +239,18 @@ func (buh *blobUploadHandler) PutBlobUploadComplete(w http.ResponseWriter, r *ht
 		return
 	}
 
+	size := buh.State.Offset
+	if offset, err := buh.Upload.Seek(0, os.SEEK_CUR); err == nil {
+		size = offset
+	}
+
 	desc, err := buh.Upload.Commit(buh, distribution.Descriptor{
 		Digest: dgst,
+		Size:   size,
 
 		// TODO(stevvooe): This isn't wildly important yet, but we should
-		// really set the length and mediatype. For now, we can let the
-		// backend take care of this.
+		// really set the mediatype. For now, we can let the backend take care
+		// of this.
 	})
 
 	if err != nil {
