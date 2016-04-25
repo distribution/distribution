fba2e3a206bdc39dbbfb57f3ec252307a720c5b9
Andrew Hsu <andrewhsu@acm.org>
2016-04-25 10:28:32 -0700

    scheme and host mandatory in baseurl
    Signed-off-by: Andrew Hsu <andrewhsu@acm.org> (github: andrewhsu)

--- docs/storage/driver/middleware/redirect/middleware_test.go
+++ docs/storage/driver/middleware/redirect/middleware_test.go
@@ -17,16 +17,11 @@ func (s *MiddlewareSuite) TestNoConfig(c *check.C) {
 	c.Assert(err, check.ErrorMatches, "no baseurl provided")
 }
 
-func (s *MiddlewareSuite) TestDefaultScheme(c *check.C) {
+func (s *MiddlewareSuite) TestMissingScheme(c *check.C) {
 	options := make(map[string]interface{})
 	options["baseurl"] = "example.com"
-	middleware, err := newRedirectStorageMiddleware(nil, options)
-	c.Assert(err, check.Equals, nil)
-
-	m, ok := middleware.(*redirectStorageMiddleware)
-	c.Assert(ok, check.Equals, true)
-	c.Assert(m.scheme, check.Equals, "https")
-	c.Assert(m.host, check.Equals, "example.com")
+	_, err := newRedirectStorageMiddleware(nil, options)
+	c.Assert(err, check.ErrorMatches, "no scheme specified for redirect baseurl")
 }
 
 func (s *MiddlewareSuite) TestHTTPS(c *check.C) {
