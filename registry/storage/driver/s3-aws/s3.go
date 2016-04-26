2dca4ad783a9e017e9fb3d0b6434743a3e7ca1da
JamesJJ <JamesJJ@users.noreply.github.com>
2016-04-26 11:56:48 +0000

    Standardise code formatting with "gofmt -s -w <file>"
    Signed-off-by: JamesJJ <JamesJJ@users.noreply.github.com>

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -55,19 +55,19 @@ var validRegions = map[string]struct{}{}
 
 //DriverParameters A struct that encapsulates all of the driver parameters after all values have been set
 type DriverParameters struct {
-	AccessKey         string
-	SecretKey         string
-	Bucket            string
-	Region            string
-	RegionEndpoint    string
-	Encrypt           bool
-	KeyID             string
-	Secure            bool
-	ChunkSize         int64
-	RootDirectory     string
-	StorageClass      string
-	UserAgent         string
-	s3ForcePathStyle  bool
+	AccessKey        string
+	SecretKey        string
+	Bucket           string
+	Region           string
+	RegionEndpoint   string
+	Encrypt          bool
+	KeyID            string
+	Secure           bool
+	ChunkSize        int64
+	RootDirectory    string
+	StorageClass     string
+	UserAgent        string
+	s3ForcePathStyle bool
 }
 
 func init() {
@@ -248,25 +248,25 @@ func FromParameters(parameters map[string]interface{}) (*Driver, error) {
 		userAgent = ""
 	}
 
-        s3ForcePathStyle := false
-        s3forcepathstyle := parameters["s3forcepathstyle"]
-        switch s3forcepathstyle := s3forcepathstyle.(type) {
-        case string:
-                b, err := strconv.ParseBool(s3forcepathstyle)
-                if err != nil {
-                        return nil, fmt.Errorf("The s3forcepathstyle parameter should be a boolean")
-                }
-                s3ForcePathStyle = b
-        case bool:
-                s3ForcePathStyle = s3forcepathstyle
-        case nil:
-                // Default to true if regionEndpoint is configured but s3forcepathstyle is not specifically set
-	        if regionEndpoint != "" {
-                        s3ForcePathStyle = true
-                }
-        default:
-                return nil, fmt.Errorf("The s3forcepathstyle parameter should be a boolean")
-        }
+	s3ForcePathStyle := false
+	s3forcepathstyle := parameters["s3forcepathstyle"]
+	switch s3forcepathstyle := s3forcepathstyle.(type) {
+	case string:
+		b, err := strconv.ParseBool(s3forcepathstyle)
+		if err != nil {
+			return nil, fmt.Errorf("The s3forcepathstyle parameter should be a boolean")
+		}
+		s3ForcePathStyle = b
+	case bool:
+		s3ForcePathStyle = s3forcepathstyle
+	case nil:
+		// Default to true if regionEndpoint is configured but s3forcepathstyle is not specifically set
+		if regionEndpoint != "" {
+			s3ForcePathStyle = true
+		}
+	default:
+		return nil, fmt.Errorf("The s3forcepathstyle parameter should be a boolean")
+	}
 
 	params := DriverParameters{
 		fmt.Sprint(accessKey),
