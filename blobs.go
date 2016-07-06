3f1434525b498c8cf24de99a916c5476a3517f7f
Michal Minář <miminar@redhat.com>
2016-07-06 16:00:29 +0200

    Export storage.CreateOptions in top-level package
    Let the options for `BlobStore.Create()` be modified in middleware
    wrappers.
    
    Signed-off-by: Michal Minar <miminar@redhat.com>

--- blobs.go
+++ blobs.go
@@ -192,6 +192,15 @@ type BlobCreateOption interface {
 	Apply(interface{}) error
 }
 
+// CreateOptions is a collection of blob creation modifiers relevant to general
+// blob storage intended to be configured by the BlobCreateOption.Apply method.
+type CreateOptions struct {
+	Mount struct {
+		ShouldMount bool
+		From        reference.Canonical
+	}
+}
+
 // BlobWriter provides a handle for inserting data into a blob store.
 // Instances should be obtained from BlobWriteService.Writer and
 // BlobWriteService.Resume. If supported by the store, a writer can be
