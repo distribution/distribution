d03aa5bc54a6faea66411e1a7b581c971a3f7888
Michal Fojtik <mfojtik@redhat.com>
2017-01-11 22:22:41 +0100

    Add more regions to registry S3 storage driver
    Namely adding ca-central-1, ap-south-1 and eu-west-1.
    
    Signed-off-by: Michal Fojtik <mfojtik@redhat.com>

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -107,7 +107,9 @@ func init() {
 		"us-west-1",
 		"us-west-2",
 		"eu-west-1",
+		"eu-west-2",
 		"eu-central-1",
+		"ap-south-1",
 		"ap-southeast-1",
 		"ap-southeast-2",
 		"ap-northeast-1",
@@ -115,6 +117,7 @@ func init() {
 		"sa-east-1",
 		"cn-north-1",
 		"us-gov-west-1",
+		"ca-central-1",
 	} {
 		validRegions[region] = struct{}{}
 	}
