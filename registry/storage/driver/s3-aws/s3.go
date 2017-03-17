45bb7c9cc90743aca8b50eb32519d405c0ef78f9
Alvin Feng <alvin4feng@yahoo.com>
2017-03-17 23:35:50 +0000

    Remove expires tag from s3 upload
    Signed-off-by: Alvin Feng <alvin4feng@yahoo.com>

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -701,15 +701,11 @@ func (d *driver) copy(ctx context.Context, sourcePath string, destPath string) e
 		return nil
 	}
 
-	// Even in the worst case, a multipart copy should take no more
-	// than a few minutes, so 30 minutes is very conservative.
-	expires := time.Now().Add(time.Duration(30) * time.Minute)
 	createResp, err := d.S3.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
 		Bucket:               aws.String(d.Bucket),
 		Key:                  aws.String(d.s3Path(destPath)),
 		ContentType:          d.getContentType(),
 		ACL:                  d.getACL(),
-		Expires:              aws.Time(expires),
 		SSEKMSKeyId:          d.getSSEKMSKeyID(),
 		ServerSideEncryption: d.getEncryptionMode(),
 		StorageClass:         d.getStorageClass(),
