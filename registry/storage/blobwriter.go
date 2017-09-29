22889ae0857096b9e85afbc487325683954f87ce
Stephen J Day <stephen.day@docker.com>
2017-09-29 13:27:49 -0700

    registry: feed the linter by removing redundant err check
    Signed-off-by: Stephen J Day <stephen.day@docker.com>
    (cherry picked from commit 4abf680c76151da0f4a9086d44abad655cb1f01e)
    Signed-off-by: Stephen J Day <stephen.day@docker.com>

--- registry/storage/blobwriter.go
+++ registry/storage/blobwriter.go
@@ -98,11 +98,7 @@ func (bw *blobWriter) Cancel(ctx context.Context) error {
 		context.GetLogger(ctx).Errorf("error closing blobwriter: %s", err)
 	}
 
-	if err := bw.removeResources(ctx); err != nil {
-		return err
-	}
-
-	return nil
+	return bw.removeResources(ctx)
 }
 
 func (bw *blobWriter) Size() int64 {
