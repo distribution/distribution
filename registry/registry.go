d260b18f2fc44a69a0d1ab865e1d889f67edb911
Olivier <o+github@gambier.email>
2018-08-10 14:58:51 -0700

    Remove ciphers that do not support perfect forward secrecy
    Signed-off-by: Olivier <o+github@gambier.email>

--- registry/registry.go
+++ registry/registry.go
@@ -139,8 +139,6 @@ func (registry *Registry) ListenAndServe() error {
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
 		}
 
