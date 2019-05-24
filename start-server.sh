45da007cf3ee2f67802093abf181dd1f36250c99
jojolocklock <jojolocklock@gmail.com>
2019-05-24 09:38:58 +0800

    remove cert print

--- start-server.sh
+++ start-server.sh
@@ -8,8 +8,6 @@ if [[ "$DOCKER_REG_AUTH_CERT" && "$REGISTRY_AUTH_TOKEN_ROOTCERTBUNDLE" ]]; then
     mkdir -p $dir
     echo "------------------------------------------------"
     echo "[INFO] writing cert to $REGISTRY_AUTH_TOKEN_ROOTCERTBUNDLE"
-    echo -e "${DOCKER_REG_AUTH_CERT}"
-    echo "------------------------------------------------"
     echo -e "${DOCKER_REG_AUTH_CERT}" > $REGISTRY_AUTH_TOKEN_ROOTCERTBUNDLE
 else
     echo "------------------------------------------------"
