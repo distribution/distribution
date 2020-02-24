672d911b66466599ec9067a1399c69084a16f339
Nikhil Bhat <nbhat510@csce-6snvrz2.cs.tamu.edu>
2020-02-24 15:23:05 -0600

    Methods for integrating recipe and db

--- encode/recipe.go
+++ encode/recipe.go
@@ -3,29 +3,39 @@ package encode
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/opencontainers/go-digest"
 )
 
-//Recipe of the image to be generated for comparision
-type Recipe struct {
+//RecipeManager of the image to be generated for comparision
+type RecipeManager struct {
+	redisPool *redis.Pool
 }
 
-//RecipePayload of the recipe structure
-type RecipePayload struct {
+//Recipe of the recipe structure
+type Recipe struct {
+	digest digest.Digest
 	Recipe []string
 }
 
-//GenerateRecipe generates a new recipe from the image address provided
-func GenerateRecipe(image string) (Recipe, error) {
-	return Recipe{}, nil
+//NewRecipeManager generates the RecipeGenerator struct
+func NewRecipeManager(redis *redis.Pool) RecipeManager {
+	return RecipeManager{
+		redisPool: redis,
+	}
 }
 
-//GetRecipeForImage generates a recipe and returns as Payload
-func GetRecipeForImage(data []byte) (RecipePayload, error) {
+//GetRecipeForLayer generates a recipe and returns as Payload
+func (rg *RecipeManager) GetRecipeForLayer(digest digest.Digest, data []byte) (Recipe, error) {
 
 	const (
 		beginIndex    = 0
-		sizeOfWindow  = 4096
-		shiftOfWindow = 1024
+		sizeOfWindow  = 1024
+		shiftOfWindow = 256
 	)
 
 	dataLength := len(data)
@@ -45,9 +55,48 @@ func GetRecipeForImage(data []byte) (RecipePayload, error) {
 		}
 		chunk := data[i:limit]
 		hashOfChunk := sha256.Sum256(chunk)
+
 		recipe[i/shiftOfWindow] = hex.EncodeToString(hashOfChunk[:])
 	}
-	return RecipePayload{
+
+	return Recipe{
+		digest: digest,
 		Recipe: recipe,
 	}, nil
+}
+
+//InsertRecipeInDB will insert the recipe in the db
+func (rg *RecipeManager) InsertRecipeInDB(recipe Recipe) error {
+	conn := rg.redisPool.Get()
+	defer conn.Close()
+
+	serialized, _ := json.Marshal(recipe)
+	i, err := conn.Do("SET", rg.generateKeyForLayer(recipe.digest), serialized)
+	fmt.Println(i)
+	fmt.Println(err)
+
+	return nil
+}
+
+//GetRecipeFromDB will insert the recipe in the db
+func (rg *RecipeManager) GetRecipeFromDB(digest digest.Digest) (Recipe, error) {
+	conn := rg.redisPool.Get()
+	defer conn.Close()
+
+	serialized, err := conn.Do("GET", rg.generateKeyForLayer(digest))
+	if err != nil {
+		return Recipe{}, err
+	}
+
+	if serialized == nil {
+		return Recipe{}, errors.New("Key not found")
+	}
+
+	var r Recipe
+	json.Unmarshal(serialized.([]byte), &r)
+	return r, err
+}
+
+func (rg *RecipeManager) generateKeyForLayer(digest digest.Digest) string {
+	return "recipe:blob:" + string(digest)
 }
