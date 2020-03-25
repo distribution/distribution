9d3ba64ffaf4789e86cc2c35cde24c751234d578
Nikhil Bhat <nbhat510@csce-6snvrz2.cs.tamu.edu>
2020-03-25 01:54:40 -0500

    Added block length as header

--- registry/client/blocks.go
+++ registry/client/blocks.go
@@ -18,13 +18,14 @@ type blocksClient struct {
 	client *http.Client
 }
 
-func (b *blocksClient) Exchange(ctx context.Context, tag digest.Digest, d encode.Declaration) (encode.BlockResponse, error) {
+func (b *blocksClient) Exchange(ctx context.Context, tag digest.Digest, d encode.Declaration) (encode.BlockResponse, int, error) {
 	ref, _ := reference.WithDigest(b.name, tag)
 	url, _ := b.ub.BuildBlocksURL(ref)
 
 	httpResponse, _ := b.client.Post(url, "application/text", strings.NewReader(d.String()))
-	length, _ := strconv.Atoi(httpResponse.Header.Get("length"))
+	headerLength, _ := strconv.Atoi(httpResponse.Header.Get("header-length"))
+	blockLength, _ := strconv.Atoi(httpResponse.Header.Get("block-length"))
 	var byteStream []byte
 	_, _ = httpResponse.Body.Read(byteStream) //Qn: ? Is it
-	return encode.GetBlockResponseFromByteStream(length, byteStream), nil
+	return encode.GetBlockResponseFromByteStream(headerLength, byteStream), blockLength, nil
 }
