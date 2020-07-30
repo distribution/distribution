a0857bbf9c4b87911e32612c7914949f7a6cff96
Wayne Warren <wayne.warren.s@gmail.com>
2020-07-30 02:20:38 +0000

    OCI: POST request with digest and blob should yield 201

--- registry/handlers/blobupload.go
+++ registry/handlers/blobupload.go
@@ -32,9 +32,9 @@ func blobUploadDispatcher(ctx *Context, r *http.Request) http.Handler {
 	}
 
 	if !ctx.readOnly {
-		handler[http.MethodPost] = http.HandlerFunc(buh.StartBlobUpload)
+		handler[http.MethodPost] = http.HandlerFunc(buh.PostBlobData)
 		handler[http.MethodPatch] = http.HandlerFunc(buh.PatchBlobData)
-		handler[http.MethodPut] = http.HandlerFunc(buh.PutBlobUploadComplete)
+		handler[http.MethodPut] = http.HandlerFunc(buh.BlobUploadComplete)
 		handler[http.MethodDelete] = http.HandlerFunc(buh.CancelBlobUpload)
 	}
 
@@ -104,6 +104,7 @@ func (buh *blobUploadHandler) StartBlobUpload(w http.ResponseWriter, r *http.Req
 	}
 
 	w.Header().Set("Docker-Upload-UUID", buh.Upload.ID())
+
 	w.WriteHeader(http.StatusAccepted)
 }
 
@@ -190,15 +191,24 @@ func (buh *blobUploadHandler) PatchBlobData(w http.ResponseWriter, r *http.Reque
 	w.WriteHeader(http.StatusAccepted)
 }
 
-// PutBlobUploadComplete takes the final request of a blob upload. The
-// request may include all the blob data or no blob data. Any data
-// provided is received and verified. If successful, the blob is linked
-// into the blob store and 201 Created is returned with the canonical
-// url of the blob.
-func (buh *blobUploadHandler) PutBlobUploadComplete(w http.ResponseWriter, r *http.Request) {
+// PostBlobData writes upload data to a blob.
+func (buh *blobUploadHandler) PostBlobData(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("digest") != "" && r.ContentLength > 0 {
+		buh.BlobUploadComplete(w, r)
+	} else {
+		buh.StartBlobUpload(w, r)
+	}
+}
+
+// BlobUploadComplete takes the final request of a blob upload. The request may
+// include all the blob data or no blob data. Any data provided is received and
+// verified. If successful, the blob is linked into the blob store and 201
+// Created is returned with the canonical url of the blob.
+func (buh *blobUploadHandler) BlobUploadComplete(w http.ResponseWriter, r *http.Request) {
+	var err error
 	if buh.Upload == nil {
-		buh.Errors = append(buh.Errors, v2.ErrorCodeBlobUploadUnknown)
-		return
+		blobs := buh.Repository.Blobs(buh)
+		buh.Upload, err = blobs.Create(buh)
 	}
 	defer buh.Upload.Close()
 
