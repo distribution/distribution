123269b1cd3d10d8e060dcb584407e6523ac8476
Collin Shoop <cshoop@digitalocean.com>
2021-06-29 08:11:01 -0400

    storagedriver/s3: Added Walk test case for dealing with errors

--- registry/storage/driver/s3-aws/s3_test.go
+++ registry/storage/driver/s3-aws/s3_test.go
@@ -2,6 +2,7 @@ package s3
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
@@ -537,6 +538,7 @@ func TestWalk(t *testing.T) {
 		return isDir
 	}
 
+	// create file structure matching fileset above
 	var created []string
 	for _, paths := range fileset {
 		for _, path := range paths {
@@ -620,6 +622,17 @@ func TestWalk(t *testing.T) {
 				"/folder1/file1",
 				// stop early
 			},
+			err: false,
+		},
+		{
+			name: "error",
+			fn: func(fileInfo storagedriver.FileInfo) error {
+				return errors.New("foo")
+			},
+			expected: []string{
+				"/",
+			},
+			err: true,
 		},
 		{
 			name: "from folder",
