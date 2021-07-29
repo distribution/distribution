c5679da3a1ed17f43fa0462d7898a4f44f937b4e
Sebastiaan van Stijn <github@gone.nl>
2021-07-29 14:51:52 +0200

    [release/2.7] vendor: github.com/golang-jwt/jwt v3.2.1
    to address CVE-2020-26160
    
    full diff: https://github.com/golang-jwt/jwt/compare/a601269ab70c205d26370c16f7c81e9017c14e04...v3.2.2
    
    3.2.1 release notes
    ---------------------------------------
    
    - Import Path Change: See MIGRATION_GUIDE.md for tips on updating your code
      Changed the import path from github.com/dgrijalva/jwt-go to github.com/golang-jwt/jwt
    - Fixed type confusion issue between string and []string in VerifyAudience.
      This fixes CVE-2020-26160
    
    3.2.2 release notes
    ---------------------------------------
    
    - Starting from this release, we are adopting the policy to support the most 2
      recent versions of Go currently available. By the time of this release, this
      is Go 1.15 and 1.16.
    - Fixed a potential issue that could occur when the verification of exp, iat
      or nbf was not required and contained invalid contents, i.e. non-numeric/date.
      Thanks for @thaJeztah for making us aware of that and @giorgos-f3 for originally
      reporting it to the formtech fork.
    - Added support for EdDSA / ED25519.
    - Optimized allocations.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- vendor/github.com/dgrijalva/jwt-go/rsa_utils.go
+++ vendor/github.com/dgrijalva/jwt-go/rsa_utils.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	ErrKeyMustBePEMEncoded = errors.New("Invalid Key: Key must be PEM encoded PKCS1 or PKCS8 private key")
+	ErrKeyMustBePEMEncoded = errors.New("Invalid Key: Key must be a PEM encoded PKCS1 or PKCS8 key")
 	ErrNotRSAPrivateKey    = errors.New("Key is not a valid RSA private key")
 	ErrNotRSAPublicKey     = errors.New("Key is not a valid RSA public key")
 )
@@ -26,6 +26,38 @@ func ParseRSAPrivateKeyFromPEM(key []byte) (*rsa.PrivateKey, error) {
 	var parsedKey interface{}
 	if parsedKey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
 		if parsedKey, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
+			return nil, err
+		}
+	}
+
+	var pkey *rsa.PrivateKey
+	var ok bool
+	if pkey, ok = parsedKey.(*rsa.PrivateKey); !ok {
+		return nil, ErrNotRSAPrivateKey
+	}
+
+	return pkey, nil
+}
+
+// Parse PEM encoded PKCS1 or PKCS8 private key protected with password
+func ParseRSAPrivateKeyFromPEMWithPassword(key []byte, password string) (*rsa.PrivateKey, error) {
+	var err error
+
+	// Parse PEM block
+	var block *pem.Block
+	if block, _ = pem.Decode(key); block == nil {
+		return nil, ErrKeyMustBePEMEncoded
+	}
+
+	var parsedKey interface{}
+
+	var blockDecrypted []byte
+	if blockDecrypted, err = x509.DecryptPEMBlock(block, []byte(password)); err != nil {
+		return nil, err
+	}
+
+	if parsedKey, err = x509.ParsePKCS1PrivateKey(blockDecrypted); err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(blockDecrypted); err != nil {
 			return nil, err
 		}
 	}
