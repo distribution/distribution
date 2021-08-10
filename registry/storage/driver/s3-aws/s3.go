941dfba40561e23fbe48a908d382e6247943252b
Collin Shoop <cshoop@digitalocean.com>
2021-08-10 12:01:36 -0400

    storagedriver/s3: Handle bulk delete partial failures as failure

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -15,6 +15,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -1000,7 +1001,7 @@ ListLoop:
 	// need to chunk objects into groups of 1000 per s3 restrictions
 	total := len(s3Objects)
 	for i := 0; i < total; i += 1000 {
-		_, err := s.DeleteObjects(&s3.DeleteObjectsInput{
+		output, err := s.DeleteObjects(&s3.DeleteObjectsInput{
 			Bucket: aws.String(d.Bucket),
 			Delete: &s3.Delete{
 				Objects: s3Objects[i:min(i+1000, total)],
@@ -1009,6 +1010,12 @@ ListLoop:
 		})
 		if err != nil {
 			return err
+		}
+		if output.Errors != nil && len(output.Errors) > 0 {
+			// ideally all errors would be returned in some way
+			// until then, at least pass back the first error message and code
+			oErr := output.Errors[0]
+			return errors.New(fmt.Sprintf("%s (%s)", *oErr.Message, *oErr.Code))
 		}
 	}
 	return nil
