9f3cbce24299fd2d452123b449d163f3bc6d09b4
Shiwei Zhang <shizh@microsoft.com>
2021-10-11 20:08:07 +0800

    fix manifest digests
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- registry/extension/distribution/manifests.go
+++ registry/extension/distribution/manifests.go
@@ -3,11 +3,13 @@ package distribution
 import (
 	"encoding/json"
 	"net/http"
+	"reflect"
 
 	"github.com/distribution/distribution/v3"
 	"github.com/distribution/distribution/v3/registry/api/errcode"
 	v2 "github.com/distribution/distribution/v3/registry/api/v2"
 	"github.com/distribution/distribution/v3/registry/extension"
+	"github.com/distribution/distribution/v3/registry/storage/driver"
 	"github.com/gorilla/handlers"
 	"github.com/opencontainers/go-digest"
 )
@@ -43,7 +45,7 @@ func (mh *manifestHandler) GetManifestDigests(w http.ResponseWriter, r *http.Req
 		return
 	}
 
-	tags, ok := mh.Repository.Tags(mh.Context).(distribution.TagManifestsProvider)
+	tags, ok := toTagManifestsProvider(mh.Repository.Tags(mh.Context))
 	if !ok {
 		mh.Errors = append(mh.Errors, errcode.ErrorCodeUnsupported.WithDetail(nil))
 		return
@@ -51,7 +53,12 @@ func (mh *manifestHandler) GetManifestDigests(w http.ResponseWriter, r *http.Req
 
 	digests, err := tags.ManifestDigests(mh.Context, tag)
 	if err != nil {
-		mh.Errors = append(mh.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
+		switch err := err.(type) {
+		case driver.PathNotFoundError:
+			mh.Errors = append(mh.Errors, v2.ErrorCodeManifestUnknown.WithDetail(map[string]string{"tag": tag}))
+		default:
+			mh.Errors = append(mh.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
+		}
 		return
 	}
 
@@ -66,4 +73,29 @@ func (mh *manifestHandler) GetManifestDigests(w http.ResponseWriter, r *http.Req
 		mh.Errors = append(mh.Errors, errcode.ErrorCodeUnknown.WithDetail(err))
 		return
 	}
+}
+
+func decomposeStruct(in interface{}) (out interface{}, ok bool) {
+	defer func() {
+		if r := recover(); r != nil {
+			out = nil
+			ok = false
+		}
+	}()
+	out = reflect.ValueOf(in).Elem().Field(0).Interface()
+	ok = true
+	return
+}
+
+func toTagManifestsProvider(i interface{}) (distribution.TagManifestsProvider, bool) {
+	tags, ok := i.(distribution.TagManifestsProvider)
+	if ok {
+		return tags, true
+	}
+	i, ok = decomposeStruct(i)
+	if !ok {
+		return nil, false
+	}
+	tags, ok = i.(distribution.TagManifestsProvider)
+	return tags, ok
 }
