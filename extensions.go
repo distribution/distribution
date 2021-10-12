1a98efa42d23cd581685623c52080ae543999892
Shiwei Zhang <shizh@microsoft.com>
2021-10-12 12:57:50 +0800

    refine ExtensionService comment
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

--- extensions.go
+++ extensions.go
@@ -11,13 +11,13 @@ type ExtensionProvider interface {
 // ExtensionService provices access to extensions.
 type ExtensionService interface {
 	// Get returns the extended service identified by its project name.
-	// The project name in the format of `<nx>/<ext>`.
+	// The project name in the format of `<ns>/<ext>`.
 	// The caller should cast the returned service into proper interface
 	// according to the extension provider.
 	Get(ctx context.Context, name string) (interface{}, error)
 
 	// All returns all available extensions with their components.
 	// The returned list consists of extension components named by
-	// `<ns>/<ext>/<component>`
+	// `_<ns>/<ext>/<component>`
 	All(ctx context.Context) ([]string, error)
 }
