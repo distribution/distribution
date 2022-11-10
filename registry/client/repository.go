bb33a43c749b5b319e11f4ef57cd4111b06cdcff
Sebastiaan van Stijn <github@gone.nl>
2022-11-10 17:30:21 +0100

    registry/client: use struct literals
    Remove some intermediate variables, and use struct literals instead.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>
    (cherry picked from commit 1d8cd5e443cad5e6d1fdee8bfdfd94b840030eee)
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- registry/client/repository.go
+++ registry/client/repository.go
@@ -135,16 +135,14 @@ func NewRepository(name reference.Named, baseURL string, transport http.RoundTri
 		return nil, err
 	}
 
-	client := &http.Client{
-		Transport:     transport,
-		CheckRedirect: checkHTTPRedirect,
-		// TODO(dmcgowan): create cookie jar
-	}
-
 	return &repository{
-		client: client,
-		ub:     ub,
-		name:   name,
+		client: &http.Client{
+			Transport:     transport,
+			CheckRedirect: checkHTTPRedirect,
+			// TODO(dmcgowan): create cookie jar
+		},
+		ub:   ub,
+		name: name,
 	}, nil
 }
 
@@ -159,16 +157,15 @@ func (r *repository) Named() reference.Named {
 }
 
 func (r *repository) Blobs(ctx context.Context) distribution.BlobStore {
-	statter := &blobStatter{
+	return &blobs{
 		name:   r.name,
 		ub:     r.ub,
 		client: r.client,
-	}
-	return &blobs{
-		name:    r.name,
-		ub:      r.ub,
-		client:  r.client,
-		statter: cache.NewCachedBlobStatter(memory.NewInMemoryBlobDescriptorCacheProvider(), statter),
+		statter: cache.NewCachedBlobStatter(memory.NewInMemoryBlobDescriptorCacheProvider(), &blobStatter{
+			name:   r.name,
+			ub:     r.ub,
+			client: r.client,
+		}),
 	}
 }
 
