ba6ab51d5168da1de9b96c01421b754d179d9e07
Dylan Scott <dscott@digitalocean.com>
2023-01-17 15:05:05 +0000

    fix: cherry-pick error

--- registry/storage/driver/walk_test.go
+++ registry/storage/driver/walk_test.go
@@ -76,113 +76,6 @@ func TestWalkFileRemoved(t *testing.T) {
 	}
 }
 
-func TestWalkFallback(t *testing.T) {
-	d := &fileSystem{
-		fileset: map[string][]string{
-			"/":        {"/file1", "/folder1", "/folder2"},
-			"/folder1": {"/folder1/file1"},
-			"/folder2": {"/folder2/file1"},
-		},
-	}
-	noopFn := func(fileInfo FileInfo) error { return nil }
-
-	tcs := []struct {
-		name     string
-		fn       WalkFn
-		from     string
-		expected []string
-		err      bool
-	}{
-		{
-			name: "walk all",
-			fn:   noopFn,
-			expected: []string{
-				"/file1",
-				"/folder1",
-				"/folder1/file1",
-				"/folder2",
-				"/folder2/file1",
-			},
-		},
-		{
-			name: "skip directory",
-			fn: func(fileInfo FileInfo) error {
-				if fileInfo.Path() == "/folder1" {
-					return ErrSkipDir
-				}
-				if strings.Contains(fileInfo.Path(), "/folder1") {
-					t.Fatalf("skipped dir %s and should not walk %s", "/folder1", fileInfo.Path())
-				}
-				return nil
-			},
-			expected: []string{
-				"/file1",
-				"/folder1", // return ErrSkipDir, skip anything under /folder1
-				// skip /folder1/file1
-				"/folder2",
-				"/folder2/file1",
-			},
-		},
-		{
-			name: "stop early",
-			fn: func(fileInfo FileInfo) error {
-				if fileInfo.Path() == "/folder1/file1" {
-					return ErrSkipDir
-				}
-				return nil
-			},
-			expected: []string{
-				"/file1",
-				"/folder1",
-				"/folder1/file1",
-				// stop early
-			},
-		},
-		{
-			name: "from folder",
-			fn:   noopFn,
-			expected: []string{
-				"/folder1/file1",
-			},
-			from: "/folder1",
-		},
-	}
-
-	for _, tc := range tcs {
-		var walked []string
-		if tc.from == "" {
-			tc.from = "/"
-		}
-		t.Run(tc.name, func(t *testing.T) {
-			err := WalkFallback(context.Background(), d, tc.from, func(fileInfo FileInfo) error {
-				walked = append(walked, fileInfo.Path())
-				if fileInfo.IsDir() != d.isDir(fileInfo.Path()) {
-					t.Fatalf("fileInfo isDir not matching file system: expected %t actual %t", d.isDir(fileInfo.Path()), fileInfo.IsDir())
-				}
-				return tc.fn(fileInfo)
-			})
-			if tc.err && err == nil {
-				t.Fatalf("expected err")
-			}
-			if !tc.err && err != nil {
-				t.Fatalf(err.Error())
-			}
-			compareWalked(t, tc.expected, walked)
-		})
-	}
-}
-
-func compareWalked(t *testing.T, expected, walked []string) {
-	if len(walked) != len(expected) {
-		t.Fatalf("Mismatch number of fileInfo walked %d expected %d; walked %s; expected %s;", len(walked), len(expected), walked, expected)
-	}
-	for i := range walked {
-		if walked[i] != expected[i] {
-			t.Fatalf("expected walked to come in order expected: walked %s", walked)
-		}
-	}
-}
-
 func TestWalkFilesFileRemoved(t *testing.T) {
 	d := &changingFileSystem{
 		fileset: []string{"zoidberg", "bender"},
@@ -341,7 +234,7 @@ func TestWalkFilesFallback(t *testing.T) {
 	}
 
 	var walked []string
-	err := WalkFilesFallback(context.Background(), d, "/", func(fileInfo FileInfo) error {
+	_, err := WalkFilesFallback(context.Background(), d, "/", func(fileInfo FileInfo) error {
 		if fileInfo.IsDir() {
 			t.Fatalf("can't walk over dir %s", fileInfo.Path())
 		}
@@ -372,7 +265,7 @@ func TestWalkFilesFallbackErr(t *testing.T) {
 	}
 
 	var walked []string
-	err := WalkFilesFallback(context.Background(), d, "/", func(fileInfo FileInfo) error {
+	_, err := WalkFilesFallback(context.Background(), d, "/", func(fileInfo FileInfo) error {
 		fmt.Println("Walk ", fileInfo.Path())
 		walked = append(walked, fileInfo.Path())
 		if fileInfo.Path() == skipFile {
