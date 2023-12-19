f23c675d85d343942a849c54fd844fac8065badd
Milos Gajdos <milosthegajdos@gmail.com>
2023-12-19 12:01:20 +0000

    release: prep for the v3 alpha-1 release
    Signed-off-by: Milos Gajdos <milosthegajdos@gmail.com>

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ var Package = "github.com/distribution/distribution/v3"
 // the latest release tag by hand, always suffixed by "+unknown". During
 // build, it will be replaced by the actual version. The value here will be
 // used if the registry is run after a go get based install.
-var Version = "v3.0.0+unknown"
+var Version = "v3.0.0-alpha.1"
 
 // Revision is filled with the VCS (e.g. git) revision being used to build
 // the program at linking time.
