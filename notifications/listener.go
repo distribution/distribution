54e5eadd49231c7efc7a2fec0e1b9c147c8cdf93
Chandon Pierre <cpierre@coreweave.com>
2026-02-18 15:34:53 -0500

    chore: inline ctx, use `WithError`
    Signed-off-by: Chandon Pierre <cpierre@coreweave.com>

--- notifications/listener.go
+++ notifications/listener.go
@@ -169,8 +169,7 @@ func (bsl *blobServiceListener) ServeBlob(ctx context.Context, w http.ResponseWr
 	if err == nil {
 		// Use a detached context for Stat() since the HTTP request context may be canceled
 		// after ServeBlob completes, but we still want to send the notification.
-		statCtx := context.WithoutCancel(ctx)
-		if desc, err := bsl.Stat(statCtx, dgst); err != nil {
+		if desc, err := bsl.Stat(context.WithoutCancel(ctx), dgst); err != nil {
 			dcontext.GetLogger(ctx).Errorf("error resolving descriptor in ServeBlob listener: %v", err)
 		} else {
 			if err := bsl.parent.listener.BlobPulled(bsl.parent.Repository.Named(), desc); err != nil {
